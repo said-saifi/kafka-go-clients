43930955ce2bdc51e29a1616fa1a665c1aa94c96
Said Saifi <said.saifi@ubanquity.com>
2021-01-03 03:45:28 +0200

    Add a sleep just to wait till consumption of kafka topic is done

--- segmentio-kafka-go/main.go
+++ segmentio-kafka-go/main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"context"
 	"log"
+	"time"
 )
 
 func main() {
+	go consume()
 	if err := produce(context.Background()); err != nil {
 		log.Fatalf("Error producing messages to kafka: %v", err)
 	}
+	time.Sleep(3*time.Second)
+
+
 }
 
 
